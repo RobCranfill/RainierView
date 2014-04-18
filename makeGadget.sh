bbd8e615e23d494586821d2864a1f8c1b8670ee3
RobCranfill <robcranfill@comcast.net>
2014-04-18 09:21:22 -0700

    Updated gadget file name to 2.0

--- makeGadget.sh
+++ makeGadget.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 # batch file to create gadget file from folder
 
-rm                        RainierView-1.0.last.gadget
-mv RainierView-1.0.gadget RainierView-1.0.last.gadget
+rm                        RainierView-2.0.last.gadget
+mv RainierView-2.0.gadget RainierView-2.0.last.gadget
 
 cd RainierView.build
-/cygdrive/c/Program\ Files/7-Zip/7z.exe a -tzip ../RainierView.gadget *
+/cygdrive/c/Program\ Files/7-Zip/7z.exe a -tzip ../RainierView-2.0.gadget *
 cd ..
 
 echo Done!
